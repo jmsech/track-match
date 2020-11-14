132614c6867aff9d816dfa16362fb1257c0aae27
jmsech <justinsech@gmail.com>
2020-11-14 16:57:21 -0500

    reseting git

--- app/spot_app.py
+++ app/spot_app.py
@@ -20,7 +20,7 @@ Run app.py
 """
 
 import os
-from flask import Flask, session, request, redirect
+from flask import Flask, session, request, redirect, render_template
 from flask_session import Session
 import spotipy
 import uuid
@@ -37,7 +37,6 @@ if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
 
 def session_cache_path():
-    print("Here:",session.get('uuid'))
     return caches_folder + session.get('uuid')
 
 @app.route('/')
@@ -58,14 +57,14 @@ def index():
     if not auth_manager.get_cached_token():
         # Step 2. Display sign in link when no token
         auth_url = auth_manager.get_authorize_url()
-        return f'<h1>See what music you have in common with Justin</h1>' \
-               f'<h2><a href="{auth_url}">Sign in</a></h2>'
+        return f'<center> '\
+                    f'<h1>See what music you have in common with Justin</h1>' \
+                    f'<h2><a href="{auth_url}">Sign in</a></h2>' \
+               f'</center>'
 
     # Step 4. Signed in, display data
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'Click <a href="/common-tracks">here</a> to see what songs you have in common. ' \
+    # return common_tracks()
+    return top_artists()
 
 @app.route('/sign_out')
 def sign_out():
@@ -77,31 +76,30 @@ def sign_out():
     session.clear()
     return redirect('/')
 
-@app.route('/common-tracks')
+@app.route('/common-tracks/')
 def common_tracks():
+
+    # Fetch Justin's library each time
+    sp_jsech = jsech_creds()
+    jsechs_tracks = get_library(sp_jsech)
+
+    # Get this users info
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
     if not auth_manager.get_cached_token():
         return redirect('/')
 
     sp = spotipy.Spotify(auth_manager=auth_manager)
-    username = sp.current_user()['display_name']
-
     tracks = get_library(sp)
-
-    with open('./data/j.sech_liked_songs.json') as f:
-        jsechs_library = json.load(f)
-    jsechs_tracks = set()
-    for item in jsechs_library:
-        jsechs_tracks.add(item['track']['id'])
+    user_id = sp.current_user()['id']
 
     common_tracks = tracks.intersection(jsechs_tracks)
 
-    playlist = sp.user_playlist_create(user='j.sech',name=f'Common Tracks -  {username} & j.sech')
+    playlist = sp_jsech.user_playlist_create(user='j.sech' ,name=f'Common Tracks -  {user_id} & j.sech')
     for i in range(0, len(common_tracks), 100):
-        sp.user_playlist_add_tracks(user='j.sech',playlist_id=playlist['uri'], tracks=list(common_tracks)[i:i+100])
+        sp_jsech.user_playlist_add_tracks(user='j.sech',playlist_id=playlist['uri'], tracks=list(common_tracks)[i:i+100])
 
     return f'<center> '\
-               f'<h2> You have {len(common_tracks)} songs in common, here they are: </h2>'\
+               f'<h2> You have {len(common_tracks)} songs in common with Justin: </h2>'\
                f'<p>(click the Spotify logo to open in Spotify and save to you library) </p>' \
                f'<iframe src="https://open.spotify.com/embed/playlist/{playlist["id"]}" width="500" height="580" frameborder="0" ' \
                f'allowtransparency="true" allow="encrypted-media"></iframe> '\
@@ -109,16 +107,83 @@ def common_tracks():
            f'</center>' \
 
 
-@app.route('/playlists')
-def playlists():
+def top_tracks():
+    # Fetch Justin's library each time
+    sp_jsech = jsech_creds()
+
+
+    # Get this users info
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
     if not auth_manager.get_cached_token():
         return redirect('/')
 
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user_playlists()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    tracks = get_library(sp)
+    user_id = sp.current_user()['id']
 
-# def top_tracks('/top-tracks')
+    jsech_top = set()
+    user_top = set()
+
+    for item in sp_jsech.current_user_top_tracks(50)['items']:
+        jsech_top.add(item['id'])
+
+    for item in sp.current_user_top_tracks(50)['items']:
+        user_top.add(item['id'])
+
+    common_top = user_top.intersection(jsech_top)
+
+    playlist = sp_jsech.user_playlist_create(user='j.sech' ,name=f'Currently Listenning -  {user_id} & j.sech')
+
+    if common_top:
+        sp_jsech.user_playlist_add_tracks(user='j.sech',playlist_id=playlist['uri'], tracks=list(common_top))
+
+        return f'<center> '\
+                   f'<h2> You and Justin share {len(common_top)} of your current favorite songs: </h2>'\
+                   f'<p>(click the Spotify logo to open in Spotify and save to you library) </p>' \
+                   f'<iframe src="https://open.spotify.com/embed/playlist/{playlist["id"]}" width="500" height="580" frameborder="0" ' \
+                   f'allowtransparency="true" allow="encrypted-media"></iframe> '\
+                   f'<h2><a href="/sign_out">[sign out]<a/></h2>' \
+               f'</center>'
+
+    return f'<center> '\
+               f'<h2> You and Justin haven\'t been listenning to any of the same songs recently :( </h2>' \
+               f'<h2><a href="/sign_out">[sign out]<a/></h2>' \
+           f'</center>'
+
+@app.route("/top_artists/")
+def top_artists():
+    # Fetch Justin's library each time
+    sp_jsech = jsech_creds()
+
+    # Get this users info
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    tracks = get_library(sp)
+    user_id = sp.current_user()['id']
+
+    jsech_top = set()
+    user_top = set()
+
+    for item in sp_jsech.current_user_top_artists(50)['items']:
+        jsech_top.add(item['name'])
+
+    for item in sp.current_user_top_artists(50)['items']:
+        user_top.add(item['name'])
+
+    common_top = user_top.intersection(jsech_top)
+
+    return render_template("list_template.html", data=[common_top, len(common_top)])
+
+
+
+def jsech_creds():
+    jsech_auth = spotipy.oauth2.SpotifyOAuth(scope='user-top-read user-library-read user-read-currently-playing playlist-modify-public',
+                                                cache_path=caches_folder + "jsech_token",
+                                                show_dialog=True)
+    return spotipy.Spotify(auth_manager=jsech_auth)
 
 # get a users liked songs
 def get_library(sp):
--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/c/Users/justi/Documents/Spotify Application/track-match/track-match"
+set -gx VIRTUAL_ENV "/mnt/c/Users/justi/Documents/Spotify Application/track-match"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
